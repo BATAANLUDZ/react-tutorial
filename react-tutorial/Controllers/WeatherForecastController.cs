105cb571a4258770e12649f39e1e0ed8d2543111
Ludwig Ivan Comiso <ludwigivan.comiso@eppi.epson.com.ph>
2023-06-05 08:43:23 +0800

    Add project files.

---
+++ react-tutorial/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace react_tutorial.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        IConfiguration _config;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var sample = _config.GetValue<string>("Logging:LogLevel:Default");
+
+            return Ok(sample);
+        }
+    }
+}
