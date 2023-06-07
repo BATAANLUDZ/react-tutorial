372756883045684ca2a48176bec6fed9997bfe53
reiji <naragmaji@gmail.com>
2023-06-07 15:52:48 +0800

    added add and update time in

--- react-tutorial/Controllers/TimeController.cs
+++ react-tutorial/Controllers/TimeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using react_tutorial.DTO;
 using react_tutorial.DTO.Time;
+using react_tutorial.Model;
 using react_tutorial.Model.Context;
 using System;
 using System.Collections.Generic;
@@ -21,7 +22,7 @@ namespace react_tutorial.Controllers
             _context = context;
         }
         [HttpGet]
-        public IActionResult Index([FromQuery]string employeeNo)
+        public IActionResult Index([FromQuery] string employeeNo)
         {
             if (string.IsNullOrEmpty(employeeNo))
                 return BadRequest();
@@ -29,22 +30,22 @@ namespace react_tutorial.Controllers
             var employee = from user in _context.TBL_M_Employees
                            where user.EmpNum == employeeNo
                            select user;
-                           
+
 
             if (!employee.Any())
                 return NotFound();
 
             var Project = (from project in _context.TBL_M_Projects
-                           select new ProjectDDLDTO 
+                           select new ProjectDDLDTO
                            { ProjectId = project.ProjectID, ProjectName = project.ProjectName })
                            .AsNoTracking().ToList();
 
             var Shifts = (from shift in _context.TBL_M_Shifts
-                         select new ShiftDDLDTO
-                         {
-                             ID = shift.ID,
-                             ShiftName = shift.SHIFTNAME
-                         }).AsNoTracking().ToList();
+                          select new ShiftDDLDTO
+                          {
+                              ID = shift.ID,
+                              ShiftName = shift.SHIFTNAME
+                          }).AsNoTracking().ToList();
 
             var timeStatus = (from log in _context.TBL_T_Logs
                               where log.EmpNum == employeeNo
@@ -60,6 +61,52 @@ namespace react_tutorial.Controllers
             };
 
             return Ok(result);
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] TimePostDTO time)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                return BadRequest(ModelState);
+            }
+
+            var employee = _context.TBL_M_Employees.Where(e => e.EmpNum == time.EmployeeNo).FirstOrDefault();
+
+            if (employee is null)
+                return NotFound();
+
+            var newLog = new TBL_T_Log()
+            {
+                EmpNum = time.EmployeeNo,
+                WorkMode = time.WorkMode,
+                Schedule = DateTime.Now.DayOfWeek.ToString(),
+                Shift = time.ShiftID,
+                LogDate = DateTime.Now,
+                TimeStatus = time.IsTimeIn ? "Time In" : "Time Out",
+                ProjectID = time.ProjectID,
+            };
+
+            if (time.IsTimeIn)
+            {
+                newLog.TimeIn = DateTime.Now.ToString("t");
+                newLog.IPAddress_1 = time.IPAddress;
+            }
+            else
+            {
+                newLog.TimeOut = DateTime.Now.ToString("t");
+                newLog.IPAddress_2 = time.IPAddress;
+                newLog.Remarks = "Present";
+            }
+
+            employee.ProjectID = time.ProjectID;
+
+            _context.TBL_T_Logs.Add(newLog);
+            _context.TBL_M_Employees.Update(employee);
+            _context.SaveChanges();
+
+
+            return Ok(newLog);
         }
 
     }
--- react-tutorial/DTO/Time/TimePostDTO.cs
+++ react-tutorial/DTO/Time/TimePostDTO.cs
@@ -11,13 +11,13 @@ namespace react_tutorial.DTO.Time
         [Required(ErrorMessage = "Employee number is required")]
         public string EmployeeNo { get; set; }
         [Required(ErrorMessage = "Status is required")]
-        public string Status { get; set; }
+        public bool IsTimeIn { get; set; }
 
         public string Remarks { get; set; }
         [Required(ErrorMessage = "Project is required")]
-        public int ProjectID { get; set; }
+        public string ProjectID { get; set; }
         [Required(ErrorMessage = "Shift is required")]
-        public int ShiftID { get; set; }
+        public string ShiftID { get; set; }
         [Required(ErrorMessage = "IP Address is required")]
         public string IPAddress { get; set; }
         [Required(ErrorMessage = "WorkMode is required")]
