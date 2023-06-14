7ee02a4c5be5338b4b7cb2f356bd40d4bba4e18a
reiji <naragmaji@gmail.com>
2023-06-14 14:58:45 +0800

    added startup jwt authorization

--- react-tutorial/Startup.cs
+++ react-tutorial/Startup.cs
@@ -37,6 +37,29 @@ namespace react_tutorial
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tutorial", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
             });
             services.AddDbContext<OAMSContext>(o => o.UseSqlServer(Configuration.GetConnectionString("OAMSConnectionString")));
 
@@ -96,6 +119,8 @@ namespace react_tutorial
             app.UseSpaStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseCors("CorsPolicy");
 
@@ -115,7 +140,8 @@ namespace react_tutorial
                     //spa.UseReactDevelopmentServer(npmScript: "dev");
                 }
             });
-            app.UseAuthentication();
+
+
         }
     }
 }
