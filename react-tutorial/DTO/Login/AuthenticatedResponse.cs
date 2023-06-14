ba438c31b2c97d69cad77f43593f4017eb77febb
reiji <naragmaji@gmail.com>
2023-06-14 14:25:59 +0800

    added model for login details and user auth

---
+++ react-tutorial/DTO/Login/AuthenticatedResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace react_tutorial.DTO.Login
+{
+    public class AuthenticatedResponse
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ react-tutorial/DTO/Login/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace react_tutorial.DTO.Login
+{
+    public class UserDTO
+    {
+        public string EmpNum { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public string Shift { get; set; }
+        public int ProjectID { get; set; }
+        public string WorkMode { get; set; }
+    }
+}
